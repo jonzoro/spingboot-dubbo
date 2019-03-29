f35067dd4ed328fcf27ff9be5149e4664f345e18
wangzh <jon-zoro@hotmail.com>
2019-03-29 17:45:41 +0800

    服务提供者可以不依赖web容器

--- dubbo-provider/src/main/java/com/example/dubboprovider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/example/dubboprovider/DubboProviderApplication.java
@@ -12,6 +12,7 @@ public class DubboProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderApplication.class, args);
+        while (true) {}
     }
 
 }
